8956255c45450f99c4f9607cd611f87ab1afb9c1
TsolmonDorjkhuu <142355898+TsolmonDorjkhuu@users.noreply.github.com>
2024-09-13 16:18:14 -0600

    Finish week 2 demos

---
+++ app/fonts/week-2/heading.js
@@ -0,0 +1,7 @@
+export default function Heading() {
+    return(
+        <heading>
+            <h1>Week 2 Demos</h1>
+        </heading>
+    );
+}
---
+++ app/fonts/week-2/page.js
@@ -0,0 +1,13 @@
+export default function Page() {
+  let a = 5;
+  let b = 10;
+
+    return (
+      <main>
+        <h1>Web dev 2 Demo</h1>
+        <p>These are demos for Web dev 2!</p>
+        <p>The sum of a a and b is {a+b}.</p>
+      </main>
+    );
+  }
+  
